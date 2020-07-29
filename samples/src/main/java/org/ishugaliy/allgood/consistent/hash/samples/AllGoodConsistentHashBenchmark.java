3851634ce2e66cc21eef2aa96bb32f6990c7b428
Harmandeep Singh <harmandeeps@qubole.com>
2020-07-29 19:05:56 +0530

    adding logic to add/removes/replace nodes

--- samples/src/main/java/org/ishugaliy/allgood/consistent/hash/samples/AllGoodConsistentHashBenchmark.java
+++ samples/src/main/java/org/ishugaliy/allgood/consistent/hash/samples/AllGoodConsistentHashBenchmark.java
@@ -2,25 +2,41 @@ package org.ishugaliy.allgood.consistent.hash.samples;
 
 import org.ishugaliy.allgood.consistent.hash.ConsistentHash;
 import org.ishugaliy.allgood.consistent.hash.HashRing;
+import org.ishugaliy.allgood.consistent.hash.hasher.DefaultHasher;
 import org.ishugaliy.allgood.consistent.hash.node.SimpleNode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.ishugaliy.allgood.consistent.hash.samples.ClusterManager.NODES_COUNT;
+import static org.ishugaliy.allgood.consistent.hash.samples.ClusterManager.NODES_DELTA;
+import static org.ishugaliy.allgood.consistent.hash.samples.ClusterManager.REQUEST_COUNT;
 
 public class AllGoodConsistentHashBenchmark
 {
-    static int REQUEST_COUNT = 10000;
     Map<String, SimpleNode> defaultKeyNodeMap;
     ConsistentHash<SimpleNode> ring;
 
     public AllGoodConsistentHashBenchmark()
     {
-        ring = HashRing.<SimpleNode>newBuilder().build();
-        ring.add(SimpleNode.of("node0"));
-        ring.add(SimpleNode.of("node1"));
-        ring.add(SimpleNode.of("node2"));
-        ring.add(SimpleNode.of("node3"));
+        // MURMUR_3,
+        //Replace node:  Miss count: 4143.0 , Miss count (%): 41.43
+        //Add node:      Miss count: 4143.0 , Miss count (%): 41.43
+        //Delete node:   Miss count: 5271.0 , Miss count (%): 52.71
+        ring = HashRing.<SimpleNode>newBuilder()
+                .hasher(DefaultHasher.MURMUR_3)
+                .partitionRate(1000)
+                .nodes(IntStream.range(0, NODES_COUNT)
+                        .mapToObj(i -> SimpleNode.of("192.168.1." + i))
+                        .collect(Collectors.toSet()))
+                .build();
+
         defaultKeyNodeMap = new HashMap<>();
 
         for(int i=0; i<REQUEST_COUNT; i++)
@@ -28,6 +44,29 @@ public class AllGoodConsistentHashBenchmark
             String key = UUID.randomUUID().toString();
             defaultKeyNodeMap.put(key, ring.locate(key).get());
         }
+    }
+
+    private void replaceNodes()
+    {
+        deleteNodes();
+        addNodes();
+    }
+
+    private void deleteNodes()
+    {
+        // remove some nodes from the ring
+        Random rand = new Random();
+        for (int i = 0; i < NODES_DELTA; i++) {
+            List<SimpleNode> nodes = new ArrayList<>(ring.getNodes());
+            ring.remove(nodes.get(rand.nextInt(nodes.size() - 1)));
+        }
+    }
+
+    private void addNodes()
+    {
+        ring.addAll(IntStream.range(NODES_COUNT, NODES_COUNT+ NODES_DELTA)
+                .mapToObj(i -> SimpleNode.of("192.168.1." + i))
+                .collect(Collectors.toList()));
     }
 
     public void RunBenchMark()
@@ -39,47 +78,38 @@ public class AllGoodConsistentHashBenchmark
         System.out.println("Guava Consistent Hashing");
         System.out.print("Replace node: ");
 
-        ring = HashRing.<SimpleNode>newBuilder().build();
-        ring.add(SimpleNode.of("node1"));
-        ring.add(SimpleNode.of("node2"));
-        ring.add(SimpleNode.of("node3"));
-        ring.add(SimpleNode.of("node4"));
+        replaceNodes();
 
         getMissCountAndPrint(defaultKeyNodeMap);
 
         System.out.println();
 
         System.out.print("Add node:     ");
-        ring = HashRing.<SimpleNode>newBuilder().build();
-        ring.add(SimpleNode.of("node0"));
-        ring.add(SimpleNode.of("node1"));
-        ring.add(SimpleNode.of("node4"));
-        ring.add(SimpleNode.of("node2"));
-        ring.add(SimpleNode.of("node3"));
+        addNodes();
         getMissCountAndPrint(defaultKeyNodeMap);
 
         System.out.println();
 
         System.out.print("Delete node:  ");
-        ring = HashRing.<SimpleNode>newBuilder().build();
-        ring.add(SimpleNode.of("node0"));
-        ring.add(SimpleNode.of("node1"));
-        ring.add(SimpleNode.of("node2"));
+        deleteNodes();
         getMissCountAndPrint(defaultKeyNodeMap);
     }
 
     private void getMissCountAndPrint(Map<String, SimpleNode> originalKeyMap)
     {
         double missCount = 0L;
+        long start = System.currentTimeMillis();
         for (String key : originalKeyMap.keySet()) {
             if (!originalKeyMap.get(key).getKey().equals(
                     ring.locate(key).get().getKey())) {
                 missCount++;
             }
         }
+        float timeTaken = (float) (System.currentTimeMillis() - start) / 1000;
 
         System.out.print(" Miss count: " + missCount);
-        System.out.print(String.format(" , Miss count (%%): %.2f", (missCount / REQUEST_COUNT) * 100));
+        System.out.print(String.format(" , Miss count (%%): %.2f, Total Time Taken: %f sec",
+                (missCount / REQUEST_COUNT) * 100, timeTaken));
     }
 
     public static void main(String[] args) {
--- samples/src/main/java/org/ishugaliy/allgood/consistent/hash/samples/ClusterManager.java
+++ samples/src/main/java/org/ishugaliy/allgood/consistent/hash/samples/ClusterManager.java
@@ -5,32 +5,75 @@ import com.google.common.hash.HashCode;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ClusterManager
 {
-    static int REQUEST_COUNT = 10000;
+    public static int REQUEST_COUNT = 10000;
+    public static int NODES_COUNT = 100;
+    public static final int NODES_DELTA = 20;
     Map<String, SimpleNode> defaultKeyNodeMap;
     List<SimpleNode> ring;
 
     public ClusterManager()
     {
-        ring = new ArrayList<>();
-        ring.add(new SimpleNode("node0", NodeState.RUNNING));
-        ring.add(new SimpleNode("node1", NodeState.RUNNING));
-        ring.add(new SimpleNode("node2", NodeState.RUNNING));
-        ring.add(new SimpleNode("node3", NodeState.RUNNING));
+        ring = IntStream.range(0, NODES_COUNT)
+                .mapToObj(i -> new SimpleNode("192.168.1." + i, NodeState.RUNNING))
+                .collect(Collectors.toList());
+
         defaultKeyNodeMap = new HashMap<>();
 
         for(int i=0; i<REQUEST_COUNT; i++)
         {
             String key = UUID.randomUUID().toString();
-            defaultKeyNodeMap.put(key, ring.get(this.getNodeIndex(ring, key, GuavaCases.SIMPLE)));
+            defaultKeyNodeMap.put(key, ring.get(this.getNodeIndex(key, GuavaCases.SIMPLE)));
         }
+    }
+
+    private void replaceNodesSimple()
+    {
+        deleteNodesSimple();
+        addNodesSimple();
+    }
+
+    private void replaceNodesRetainNodes()
+    {
+        deleteNodesRetainNodes();
+        addNodesSimple();
+    }
+
+    private void deleteNodesSimple()
+    {
+        // remove some nodes from the ring
+        Random rand = new Random();
+        for (int i = 0; i < NODES_DELTA; i++) {
+            ring.remove(ring.get(rand.nextInt(ring.size() - 1)));
+        }
+    }
+
+    private void deleteNodesRetainNodes()
+    {
+        // remove some nodes from the ring
+        Random rand = new Random();
+        for (int i = 0; i < NODES_DELTA; i++) {
+            int nodeIndex = rand.nextInt(ring.size() - 1);
+            SimpleNode changeNode = ring.remove(nodeIndex);
+            changeNode.setNodeState(NodeState.STOPPED);
+            ring.add(nodeIndex, changeNode);
+        }
+    }
+
+    private void addNodesSimple()
+    {
+        ring.addAll(IntStream.range(NODES_COUNT, NODES_COUNT+ NODES_DELTA)
+                .mapToObj(i -> new SimpleNode("192.168.1." + i, NodeState.RUNNING))
+                .collect(Collectors.toList()));
     }
 
     private void GuavaConsistentHashing()
@@ -42,31 +85,19 @@ public class ClusterManager
         System.out.println("Guava Consistent Hashing");
 
         System.out.print("Replace node: ");
-        ring.clear();
-        ring.add(new SimpleNode("node1", NodeState.RUNNING));
-        ring.add(new SimpleNode("node2", NodeState.RUNNING));
-        ring.add(new SimpleNode("node3", NodeState.RUNNING));
-        ring.add(new SimpleNode("node4", NodeState.RUNNING));
+        replaceNodesSimple();
         getMissCountAndPrint(GuavaCases.SIMPLE);
 
         System.out.println();
 
         System.out.print("Add node:     ");
-        ring.clear();
-        ring.add(new SimpleNode("node0", NodeState.RUNNING));
-        ring.add(new SimpleNode("node1", NodeState.RUNNING));
-        ring.add(new SimpleNode("node4", NodeState.RUNNING));
-        ring.add(new SimpleNode("node2", NodeState.RUNNING));
-        ring.add(new SimpleNode("node3", NodeState.RUNNING));
+        addNodesSimple();
         getMissCountAndPrint(GuavaCases.SIMPLE);
 
         System.out.println();
 
         System.out.print("Delete node:  ");
-        ring.clear();
-        ring.add(new SimpleNode("node0", NodeState.RUNNING));
-        ring.add(new SimpleNode("node1", NodeState.RUNNING));
-        ring.add(new SimpleNode("node2", NodeState.RUNNING));
+        deleteNodesSimple();
         getMissCountAndPrint(GuavaCases.SIMPLE);
     }
 
@@ -79,33 +110,19 @@ public class ClusterManager
         System.out.println("Retain Nodes Consistent Hashing");
         System.out.print("Replace node: ");
 
-        ring.clear();
-        ring.add(new SimpleNode("node0", NodeState.STOPPED));
-        ring.add(new SimpleNode("node1", NodeState.RUNNING));
-        ring.add(new SimpleNode("node2", NodeState.RUNNING));
-        ring.add(new SimpleNode("node3", NodeState.RUNNING));
-        ring.add(new SimpleNode("node4", NodeState.RUNNING));
+        replaceNodesRetainNodes();
         getMissCountAndPrint(GuavaCases.RETAINING_NODES);
 
         System.out.println();
 
         System.out.print("Add node:     ");
-        ring.clear();
-        ring.add(new SimpleNode("node0", NodeState.RUNNING));
-        ring.add(new SimpleNode("node1", NodeState.RUNNING));
-        ring.add(new SimpleNode("node4", NodeState.RUNNING));
-        ring.add(new SimpleNode("node2", NodeState.RUNNING));
-        ring.add(new SimpleNode("node3", NodeState.RUNNING));
+        addNodesSimple();
         getMissCountAndPrint(GuavaCases.RETAINING_NODES);
 
         System.out.println();
 
         System.out.print("Delete node:  ");
-        ring.clear();
-        ring.add(new SimpleNode("node0", NodeState.RUNNING));
-        ring.add(new SimpleNode("node1", NodeState.RUNNING));
-        ring.add(new SimpleNode("node2", NodeState.RUNNING));
-        ring.add(new SimpleNode("node3", NodeState.STOPPED));
+        deleteNodesRetainNodes();
         getMissCountAndPrint(GuavaCases.RETAINING_NODES);
     }
 
@@ -121,36 +138,44 @@ public class ClusterManager
     private void getMissCountAndPrint(GuavaCases guavaCase)
     {
         double missCount = 0L;
+        long start = System.currentTimeMillis();
         for (String key : defaultKeyNodeMap.keySet()) {
             if (!defaultKeyNodeMap.get(key).getName().equals(
-                    ring.get(getNodeIndex(ring, key, guavaCase)).getName())) {
+                    ring.get(getNodeIndex(key, guavaCase)).getName())) {
                 missCount++;
             }
         }
+        float timeTaken = (float) (System.currentTimeMillis() - start) / 1000;
 
         System.out.print(" Miss count: " + missCount);
-        System.out.print(String.format(" , Miss count (%%): %.2f", (missCount / REQUEST_COUNT) * 100));
+        System.out.print(String.format(" , Miss count (%%): %.2f, Total Time Taken: %f sec",
+                (missCount / REQUEST_COUNT) * 100, timeTaken));
     }
 
-    public int getNodeIndex(List<SimpleNode> stateRing, String key, GuavaCases guavaCase)
+    public int getNodeIndex(String key, GuavaCases guavaCase)
     {
         HashFunction hf = Hashing.md5();
         hf = Hashing.hmacMd5(hf.hashString(key, Charsets.UTF_8).asBytes());
         HashCode hc = hf.hashString(key, Charsets.UTF_8);
-        int nodeIndex = Hashing.consistentHash(hc, stateRing.size());
+        int nodeIndex = Hashing.consistentHash(hc, ring.size());
 
         if (guavaCase.equals(GuavaCases.SIMPLE)) {
             return nodeIndex;
         }
 
         if (hc.asInt() % 2 == 0) {
-            while(!stateRing.get(nodeIndex).getNodeState().equals(NodeState.RUNNING)) {
-                nodeIndex = (nodeIndex+1)%stateRing.size();
+            while(!ring.get(nodeIndex).getNodeState().equals(NodeState.RUNNING)) {
+                //System.out.println(" increasing " + ring.get(nodeIndex).getName());
+                nodeIndex = (nodeIndex+1)%ring.size();
             }
         }
         else {
-            while(!stateRing.get(nodeIndex).getNodeState().equals(NodeState.RUNNING)) {
-                nodeIndex = Math.abs(nodeIndex-1)%stateRing.size();
+            while(!ring.get(nodeIndex).getNodeState().equals(NodeState.RUNNING)) {
+                //System.out.println(" decreasing " + ring.get(nodeIndex).getName());
+                if (nodeIndex == 0) {
+                    nodeIndex = ring.size();
+                }
+                nodeIndex = (nodeIndex-1)%ring.size();
             }
         }
         return nodeIndex;
@@ -187,6 +212,11 @@ public class ClusterManager
         public String getName()
         {
             return name;
+        }
+
+        public void setNodeState(NodeState nodeState)
+        {
+            this.nodeState = nodeState;
         }
 
         @Override
